16d133557fb452422a33bf9ad34ef11c05e23c80
Roy <Roy.Rosenberg@live.com>
2020-05-08 19:04:44 +0300

    added some componenents

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,15 @@
 import { NgModule } from '@angular/core';
-import { Routes, RouterModule } from '@angular/router';
+import { RouterModule, Routes } from '@angular/router';
+
+import { ProductsComponent } from './pages/products/products.component';
+import { StamComponent } from './pages/stam/stam.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '',   redirectTo: '/stam', pathMatch: 'full' },
+  { path: 'stam', component: StamComponent },
+  { path: 'products', component: ProductsComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,18 @@
-import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { StoreModule } from '@ngrx/store';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { StoreModule } from '@ngrx/store';
-import { reducers, metaReducers } from './reducers';
+import { MenuComponent } from './pages/menu/menu.component';
+import { ProductsComponent } from './pages/products/products.component';
+import { metaReducers, reducers } from './reducers';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MenuComponent,
+    ProductsComponent
   ],
   imports: [
     BrowserModule,
