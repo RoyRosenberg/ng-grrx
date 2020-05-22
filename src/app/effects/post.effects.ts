a10c033ad7e2bd7c2462e956a2bcb91dce106d44
Roy <Roy.Rosenberg@live.com>
2020-05-22 14:03:31 +0300

    implement fork join

--- src/app/effects/post.effects.ts
+++ src/app/effects/post.effects.ts
@@ -1,26 +1,43 @@
 import { Injectable } from '@angular/core';
 import { Actions, Effect, ofType } from '@ngrx/effects';
-import { of } from 'rxjs';
+import { Store } from '@ngrx/store';
+import { forkJoin, of } from 'rxjs';
 import { catchError, map, mergeMap, tap } from 'rxjs/operators';
 import { PostService } from 'src/services/post.service';
+import { UserService } from 'src/services/user.service';
 
+import { State } from '../state/app.state';
 import { LoadPostsFailed, LoadPostsSuccess, PostActionTypes } from '../state/post.actions';
 
 @Injectable()
 export class PostEffects {
     constructor(
         private actions$: Actions,
-        private postService: PostService
+        private postService: PostService,
+        private userService: UserService,
+        private store: Store<State>
     ) { }
 
     @Effect()
     loadPosts$ = this.actions$.pipe(
-        tap(() => console.log('In Effect')),
+        tap(() => console.log('In posts Effect')),
         ofType(PostActionTypes.GetPosts),
         mergeMap(action =>
-            this.postService.getPosts()
+            forkJoin(
+                this.postService.getPosts(),
+                this.userService.getUsers())
                 .pipe(
-                    map(posts => new LoadPostsSuccess(posts)),
+                    map(res => {
+                        const [posts, users] = res;
+                        posts.forEach(p => {
+                            const userList = users.filter(u => u.id === p.userId);
+                            if (userList.length > 0) {
+                                p.user = userList[0];
+                            }
+                        });
+                        return new LoadPostsSuccess(posts);
+                    }),
+                    // map(res => new LoadPostsSuccess(res)),
                     catchError((err) => {
                         console.log(err);
                         return of(new LoadPostsFailed());
--- src/app/models/post.ts
+++ src/app/models/post.ts
@@ -1,6 +1,9 @@
+import { User } from './user';
+
 export class Post{
     id: number;
     title: string;
     body: string;
     userId: number;
+    user: User;
 }
